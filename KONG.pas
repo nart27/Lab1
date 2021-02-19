5055c89c4531e9aca6f4d2a3ece82602afc916aa
Yury Karpenko <nart27@gmail.com>
2021-02-19 11:35:30 +0300

    Update KONG.pas
    Some improvements in pascal version

--- KONG.pas
+++ KONG.pas
@@ -1,17 +1,31 @@
-﻿program KONG;
+program KONG;
+
+Uses DateUtils;
 
 var
   x: longword = 10;
-  m:real;
-function rand:longword;
+  m: longword;
+ 
+procedure init();
 begin
-  m:=(exp(32*ln(2)));
-  m:=round(m);
+  m:=Round(exp(32*ln(2))-1);
+end;
+function rand():longword;
+begin
   x:=(1664525*x+1013904223) mod m;
   rand:=x;
 end;
 
+procedure test();
+  var i:integer;
+begin
+  for i:=1 to 10 do
+  begin
+	writeln('Rand value is ', rand());
+  end;
+end;
 
 begin
-  writeln(rand);  
+  init();
+  test();
 end.
